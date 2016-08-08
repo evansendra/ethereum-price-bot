ce907a0e6f465a5282d7a528d3cabe7e9f86d068
Evan <code@evansendra.com>
2016-08-08 22:11:12 +0200

    post first tweet updating ethereum price

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import api
+import tweepy
+import os
 
-print(api.getEthPriceInUsd())
+CONSUMER_KEY=os.environ.get('ETHPRICE_CONSUMER_KEY')
+CONSUMER_SECRET=os.environ.get('ETHPRICE_CONSUMER_SECRET')
+ACCESS_TOKEN=os.environ.get('ETHPRICE_ACCESS_TOKEN')
+ACCESS_SECRET=os.environ.get('ETHPRICE_ACCESS_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+tapi = tweepy.API(auth)
+tapi.update_status("$" + str(api.getEthPriceInUsd()))
